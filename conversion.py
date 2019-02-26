2994248e343dc476a9aee82cbf186411d498422a
saichandrakumar <saichabdra1128@gmail.com>
2019-02-27 00:29:22 +0530

    add exceptionss

--- conversion.py
+++ conversion.py
@@ -1,22 +1,32 @@
 import pdfkit
 import easygui
 import os
-from pathlib import Path
+# from pathlib import Path
 import pdftotext
 
-a = input("Enter the input file::")
-b = input("Enter the output path (Ex:/home/john/MySampleProject/)::")
-pdf_filename = Path(a).stem
-op = pdf_filename+".html"
-html_file_path = b + op
 
-if os.path.exists(a):
-    os.system("pdftotext '%s' '%s' " % (a,html_file_path))
-    pdfkit.from_file(html_file_path, pdf_filename+'1.pdf')
-    print("Created File Location:")
-    print(b+ pdf_filename+ '1.pdf')
+
+pdf_file_path = input("Enter the input pdf file path::")
+if os.path.exists(pdf_file_path):
+    path, pdf_filename = os.path.split(pdf_file_path)
+    (name,ext) = os.path.splitext(pdf_filename)
+    if(ext ==  '.pdf'):
+        op = name+".html"
+        tp = name+"1.pdf"
+        html_file_path = path+'/' + op
+        out_pdf = path +'/'+ tp   
+        os.system("pdftotext '%s' '%s' " % (pdf_file_path, html_file_path))
+        pdfkit.from_file(html_file_path, out_pdf)
+        print("Converted html file path:")
+        print(html_file_path)
+        print("Output file location:")
+        print(out_pdf) 
+    else:
+        print ("Error : Please check your specified path.")
 else:
-    print ("Error : PDF file not exists in specified path")
+    print ("Error : PDF file not exists in specified path.")
+
+
 
 
 
--- remove_empty.py
+++ remove_empty.py
@@ -1,35 +1,39 @@
 import json
+import os
 
-f = open('data.json')
-data = json.load(f)
-f.close()
-
-print("Input:" + str(data))
-
-def dictCheck(dkey,dvalue):
-    if not dvalue:
-        del data[kst]
-        return
-    elif dvalue == '':
-        del data[kst][dkey]
-        return
-    elif isinstance(dvalue,dict):
-        for s,t in dvalue.items():
-            if isinstance(t,dict):
-                dictCheck(s,t)
-            else:
-                if dvalue[s] == "":
-                    del dvalue[s]
-                    return
-
-for k, v in list(data.items()):
-    kst = k
-    if not v:
-        del data[k]
-    elif isinstance(v,dict):
-        dictCheck(k,v)
-
-print("Output: "+ str(data))
+filepath = input("Enter the input json file path::")
+try:
+    f = open(filepath)
+    try:
+        data = json.load(f)
+        f.close()
+        print("Input:" + str(data))
+        def dictCheck(dkey,dvalue):
+            if not dvalue:
+                del data[kst]
+                return
+            elif dvalue == '':
+                del data[kst][dkey]
+                return
+            elif isinstance(dvalue,dict):
+                for s,t in dvalue.items():
+                    if isinstance(t,dict):
+                        dictCheck(s,t)
+                    else:
+                        if dvalue[s] == "":
+                            del dvalue[s]
+                            return
+        for k, v in list(data.items()):
+            kst = k
+            if not v:
+                del data[k]
+            elif isinstance(v,dict):
+                dictCheck(k,v)
+        print("Output: "+ str(data))
+    except AttributeError:
+        print("Oops! Please check your Json data.  Try again...")
+except:
+    print("Oops! Unable to open Json file.  Try again...")
 
 
 
