f4e13400992951846868a2037026f24dfdea4abe
saichandrakumar <saichabdra1128@gmail.com>
2019-02-26 11:16:04 +0530

    add conversion.py file changes

--- conversion.py
+++ conversion.py
@@ -1,18 +1,28 @@
 import pdfkit
 import easygui
+import os
 from pathlib import Path
-import pdb
+import pdftotext
+
+a = input("Enter the input file::")
+b = input("Enter the output path (Ex:/home/john/MySampleProject/)::")
+pdf_filename = Path(a).stem
+op = pdf_filename+".html"
+html_file_path = b + op
+
+if os.path.exists(a):
+    os.system("pdftotext '%s' '%s' " % (a,html_file_path))
+    pdfkit.from_file(html_file_path, pdf_filename+'1.pdf')
+    print("Created File Location:")
+    print(b+ pdf_filename+ '1.pdf')
+else:
+    print ("Error : PDF file not exists in specified path")
 
 
-# pdf2txt.py test.pdf -t html -o test.html
-pdf2txt.py -o output.html -t html test.pdf
-filepath = easygui.fileopenbox()
-filename = Path(filepath).stem
-pdfkit.from_file(filepath, filename +'.pdf')
-filename = Path(filepath)
-location = filename.with_suffix('.pdf')
-print("Created File Location:")
-print(location)
+
+
+
+
 
 
 
